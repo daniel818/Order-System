d771599fbbe057188ea57e64176ac1aea750182d
daniel818 <avital.daniel@gmail.com>
2017-09-08 15:19:01 +0300

    full project

---
+++ src/base.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Daniel on 04/09/2017.
+ */
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyBU4hRUMvPqsYDhWdF8zRnmYVMwR5NUISw",
+    authDomain: "catch-of-the-day-abut.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-abut.firebaseio.com",
+});
+
+export default base;
